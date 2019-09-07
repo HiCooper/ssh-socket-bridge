b3af0d39295d294acebe8910c8b4b770f7c6939f
HiCooper <294237781@qq.com>
2019-09-07 22:59:39 +0800

    xx

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-var app = require('express')();
-var httpServer = require('http').Server(app);
-var socketIo = require('socket.io')(httpServer);
-var utf8 = require('utf8');
-var SSHClient = require('ssh2').Client;
-var localSSHPrivateKey = require('fs').readFileSync('/Users/xueancao/.ssh/id_rsa');
-var localSSHPassphrase = null;
+const app = require('express')();
+const httpServer = require('http').Server(app);
+const socketIo = require('socket.io')(httpServer);
+const utf8 = require('utf8');
+const SSHClient = require('ssh2').Client;
+const localSSHPrivateKey = require('fs').readFileSync('/Users/xueancao/.ssh/id_rsa');
+const localSSHPassphrase = null;
 
 function SSHServer(machineConfig, socket) {
-    var ssh = new SSHClient();
-    let { channelId, ip, username, password } = machineConfig;
+    const ssh = new SSHClient();
+    let {msgId, ip, username, password} = machineConfig;
     ssh.on('ready', function () {
-        console.log('客户端:' + channelId + ',登陆成功->' + username + '@'+ ip)
-        socket.emit(channelId, '\r\n***' + ip + ' SSH CONNECTION ESTABLISHED ***\r\n');
+        console.log('客户端:' + msgId + ',登陆成功->' + username + '@' + ip);
+        socket.emit(msgId, '\r\n***' + ip + ' SSH CONNECTION ESTABLISHED ***\r\n');
         ssh.shell(function (err, stream) {
             if (err) {
-                return socket.emit(channelId, '\r\n' + err.message + '\r\n');
+                return socket.emit(msgId, '\r\n' + err.message + '\r\n');
             }
-            socket.on(channelId, function (data) {
+            socket.on(msgId, function (data) {
                 stream.write(data);
             });
             stream.on('data', function (d) {
-                socket.emit(channelId, utf8.decode(d.toString('binary')));
+                socket.emit(msgId, utf8.decode(d.toString('binary')));
             }).on('close', function (val) {
                 ssh.end();
             });
         })
     }).on('close', function () {
-        console.log('客户端：' + channelId + '登出...\r\n')
-        socket.emit(channelId, '\r\n*** SSH CONNECTION CLOSED ***\r\n');
+        console.log('客户端：' + msgId + '登出...\r\n');
+        socket.emit(msgId, '\r\n*** SSH CONNECTION CLOSED ***\r\n');
     }).on('error', function (err) {
         console.log(err.message);
-        socket.emit(channelId, '\r\n' + err.message + '\r\n');
+        socket.emit(msgId, '\r\n' + err.message + '\r\n');
     }).connect({
         host: ip,
         port: 22,
@@ -43,17 +43,16 @@ function SSHServer(machineConfig, socket) {
     });
 }
 
-
 socketIo.on('connection', function (socket) {
     socket.on('SSHServer', function (machineConfig) {
         SSHServer(machineConfig, socket);
-    })
+    });
 
-    socket.on('disconnect', function (msg) {
+    socket.on('disconnect', function () {
         console.log('client disconnect');
     });
-})
+});
 
 httpServer.listen(8000, function () {
     console.log('listening on * 8000');
-})
+});
